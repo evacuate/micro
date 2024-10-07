f6c2ea2a82605700f34017094cfef6628684005c
minagishl <mvxd568.co@icloud.com>
2024-10-07 15:03:26 +0900

    Adding a build script

---
+++ scripts/build.mjs
@@ -0,0 +1,19 @@
+import { build } from "esbuild";
+import path from "node:path";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const options = {
+  entryPoints: [path.resolve(__dirname, "../src/index.ts")],
+  outfile: path.resolve(__dirname, "../dist/index.cjs"),
+  platform: "node",
+  format: "cjs",
+  bundle: true,
+  minify: true,
+  external: ["newrelic"],
+};
+
+build(options).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
