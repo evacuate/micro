055284463423658848f1b07793d3d8882b133c26
minagishl <mvxd568.co@icloud.com>
2024-10-10 00:58:56 +0900

    Correction of hard-to-see time variables

--- src/messages/create.ts
+++ src/messages/create.ts
@@ -12,7 +12,8 @@ export function createEarthquakeMessage(
   isDev: boolean,
 ): Body {
   const newTime = new Date(time);
-  const receivedTime = `${newTime.getHours()}:${newTime.getMinutes()}:${newTime.getSeconds()} on ${newTime.getFullYear()}/${newTime.getMonth()}/${newTime.getDate()}`;
+  const formattedTime = `${newTime.getHours()}:${String(newTime.getMinutes()).padStart(2, '0')}:${String(newTime.getSeconds()).padStart(2, '0')}`;
+  const formattedDate = `${newTime.getFullYear()}/${String(newTime.getMonth() + 1).padStart(2, '0')}/${String(newTime.getDate()).padStart(2, '0')}`;
 
   const descriptionPrefix = isDev
     ? 'This information is a test distribution\n'
@@ -20,7 +21,7 @@ export function createEarthquakeMessage(
 
   const body = {
     title: 'Earthquake Information',
-    description: `${descriptionPrefix}Maximum intensity ${scale} was received at ${receivedTime}.`,
+    description: `${descriptionPrefix}Maximum intensity ${scale} was received at ${formattedTime} on ${formattedDate}.`,
     fields: points.map(([scale, regions]) => ({
       name: `Seismic Intensity ${scale}`,
       value: Array.from(regions).join(', '),
