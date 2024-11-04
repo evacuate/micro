22edd4d4beb13703baa03b8239cfbdd0139bb6fa
みなぎし <mvxd568.co@icloud.com>
2024-11-04 07:08:46 +0000

    Fixed errors in code formatter

--- src/messages/handle.ts
+++ src/messages/handle.ts
@@ -1,8 +1,8 @@
+import { createEarthquakeMessage } from '~/messages/create';
+import sendMessage from '~/messages/send';
 import parsePoints from '~/parsers/points';
 import parseScale from '~/parsers/scale';
 import type { JMAQuake } from '~/types';
-import { createEarthquakeMessage } from '~/messages/create';
-import sendMessage from '~/messages/send';
 
 export async function handleEarthquake(
   earthquakeData: JMAQuake,
