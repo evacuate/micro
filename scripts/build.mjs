4942d7ae95cab9be8921a55ed15a9503f1c58d4b
minagishl <mvxd568.co@icloud.com>
2025-01-17 04:32:42 +0900

    chore: improve code readability

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,18 +1,14 @@
-import { build } from "esbuild";
-import path from "node:path";
+import { build } from 'esbuild';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const dir = dirname(fileURLToPath(import.meta.url));
 
-const options = {
-  entryPoints: [path.resolve(__dirname, "../src/index.ts")],
-  outfile: path.resolve(__dirname, "../dist/index.cjs"),
-  platform: "node",
-  format: "cjs",
+build({
+  entryPoints: [resolve(dir, '../src/index.ts')],
+  outfile: resolve(dir, '../dist/index.cjs'),
+  platform: 'node',
+  format: 'cjs',
   bundle: true,
   minify: true,
-};
-
-build(options).catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+}).catch(() => process.exit(1));
